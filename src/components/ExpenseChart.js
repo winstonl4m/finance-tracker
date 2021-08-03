e23750bb35906cdb21a7fccfa5a5e0606904e36e
winstonl4m <34150375+winstonl4m@users.noreply.github.com>
2021-08-02 17:01:28 -0700

    pie chart
    -dynamically changing pie chart when entering an expense item or deleting an expense item

--- src/components/ExpenseChart.js
+++ src/components/ExpenseChart.js
@@ -1,4 +1,4 @@
-import React,{useContext, useState} from 'react'
+import React,{useContext, useState, useEffect} from 'react'
 import Chart from "react-google-charts";
 import {AppContext} from '../context/AppContext';
 import ExpenseList from './ExpenseList';
@@ -9,13 +9,91 @@ const ExpenseChart = ({filteredExpenses}) =>{
     //const {expenses} = useContext(AppContext);
 
 
-    const [expenseChartEntry, setExpenseChartEntry] = useState(filteredExpenses);
+    // gets all expenses in array
+    const filteredCatEx = filteredExpenses.filter(expense => expense.ex == true);
+
+
+    const [expenseChartEntry, setExpenseChartEntry] = useState(filteredCatEx);
+    
+    useEffect(() => {
+        setExpenseChartEntry(filteredCatEx)
+        
+    }, [filteredCatEx])
+
+    // filters rent category
+    var rentArr = expenseChartEntry.filter(function(expense){
+        return expense.cat == 'Rent';
+    });
+
+    // gets total $ spent on rent
+    const rentTotal = rentArr.reduce((rentTotal, item) =>{
+        return (rentTotal +=item.cost);
+    }, 0);
+
+    var foodArr = expenseChartEntry.filter(function(expense){
+        return expense.cat == 'Food';
+    });
+
+    const foodTotal = foodArr.reduce((foodTotal, item) =>{
+        return (foodTotal +=item.cost);
+    }, 0);
+
+    var utlityArr = expenseChartEntry.filter(function(expense){
+        return expense.cat == 'Utilities';
+    });
+
+    const utlityTotal = utlityArr.reduce((utlityTotal, item) =>{
+        return (utlityTotal +=item.cost);
+    }, 0);
+
+    var savingsArr = expenseChartEntry.filter(function(expense){
+        return expense.cat == 'Savings';
+    });
+
+    const savingsTotal = savingsArr.reduce((savingsTotal, item) =>{
+        return (savingsTotal +=item.cost);
+    }, 0);
+
+
+    var lifestyleArr = expenseChartEntry.filter(function(expense){
+        return expense.cat == 'Lifestyle';
+    });
+
+    const lifestyleTotal = lifestyleArr.reduce((lifestyleTotal, item) =>{
+        return (lifestyleTotal +=item.cost);
+    }, 0);
+
+
+    var recreationArr = expenseChartEntry.filter(function(expense){
+        return expense.cat == 'Recreation';
+    });
+
+    const recreationTotal = recreationArr.reduce((recreationTotal, item) =>{
+        return (recreationTotal +=item.cost);
+    }, 0);
+
+
+    var miscArr = expenseChartEntry.filter(function(expense){
+        return expense.cat == 'Miscellaneous';
+    });
+
+    const miscTotal = miscArr.reduce((miscTotal, item) =>{
+        return (miscTotal +=item.cost);
+    }, 0);
+
+
+    const data = [
+        ['Category', '$ Spent'],
+        ['Rent', rentTotal],
+        ['Food', foodTotal],
+        ['Utility', utlityTotal],
+        ['Savings', savingsTotal],
+        ['Lifestyle', lifestyleTotal],
+        ['Recreation',recreationTotal],
+        ['Miscellaneous', miscTotal],
+    ]
 
     
-    
-
-
-
 
 
     return (
@@ -25,17 +103,9 @@ const ExpenseChart = ({filteredExpenses}) =>{
                 height={'300px'}
                 chartType="PieChart"
                 loader={<div>Loading Chart</div>}
-                data={[
-                    ['Category', '$ Spent'],
-                    ['Rent', 11],
-                    ['Food', 2],
-                    ['Utility', 2],
-                    ['Savings', 2],
-                    ['Lifestyle', 7],
-                    ['Recreation',10],
-                ]}
+                data={data}
                 options={{
-                    title: 'My Finance Spread',
+                    title: 'My Expense Spread',
                 }}
                 rootProps={{ 'data-testid': '1' }}
                 />
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -6,15 +6,38 @@ import {AppContext} from '../context/AppContext';
 const ExpenseTotal = () =>{
     const {expenses} = useContext(AppContext);
 
+    var incomeArr = expenses.filter(function(expense){
+        return expense.ex == false;
+    });
+
+    var expenseArr = expenses.filter(function(expense){
+        return expense.ex == true;
+    });
+
+    const incomeTotal = incomeArr.reduce((incomeTotal, item) =>{
+        return (incomeTotal +=item.cost);
+    }, 0);
+
+    const expenseTotal = expenseArr.reduce((expenseTotal, item) =>{
+        return (expenseTotal +=item.cost);
+    }, 0);
+
     const total = expenses.reduce((total, item) =>{
         return (total +=item.cost);
     }, 0);
 
 
     return (
-        <div className = 'alert alert-primary'>
-            <span> Spent so far : ${total}</span>
+        <>
+        <div className = 'alert alert-danger'>
+            <span> Spent so far : ${expenseTotal}</span>
         </div>
+
+        <div className = 'alert alert-primary'>
+            <span> Saved so far : ${-1*incomeTotal}</span>
+        </div>
+        </>
+
     );
 };
 
